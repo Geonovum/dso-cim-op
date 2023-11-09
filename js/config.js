8449805d7b2c916b38aae2b3d5f1ad132c353e64
Paul Janssen <p.janssen@geonovum.nl>
2023-11-09 16:30:01 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -4,8 +4,8 @@ let respecConfig = {
   // title is verplicht! Neem hier de titel van het document op ----------------------
   title: "Conceptueel Informatiemodel Officiële Publicaties binnen de context van de DSO-LV",
   //-- specStatus is verplicht! (activeer 1 van de volgende) --------------------------
-  specStatus: "wv",                   // Werkversie
-  //specStatus: "cv",               // Consultatieversie
+ // specStatus: "wv",                   // Werkversie
+  specStatus: "cv",               // Consultatieversie
   //specStatus: "vv",               // Versie ter vaststelling
   //specStatus: "def",              // Vastgestelde versie
   //specStatus: "basis",            // Basis Document
