bd0a9af16706fb7c3fec27d6af6634b610bd421a
Paul Janssen <p.janssen@geonovum.nl>
2023-11-09 16:33:54 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -22,7 +22,7 @@ let respecConfig = {
   shortName: "dso-cim-op",
   //-- publishDate is verplicht -------------------------------------------------------
   //-- NB: in de werkversie uitzetten, want dan pakt Respec de pushdate ---------------
-  //publishDate: "2023-03-28",
+  publishDate: "2023-11-09",
   //eventueel is het mogelijk een versienummer mee te geven, maar bij Geonovum werken we gewoonlijk alleen met datum als onderdeel van de permanente URI.
   //publishVersion: "0.0.2",
   //previousVersion: "0.0.1",
