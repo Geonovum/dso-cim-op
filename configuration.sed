76a30e06f5a64185f316ded33d96df8c3d861e08
Wilko Quak <w.quak@geonovum.nl>
2025-04-18 18:03:03 +0200

    remove bootstrap scripts

--- configuration.sed
+++
@@ -1,4 +0,0 @@
-s/@STANDARD@/CIM-OP/g
-s/@MODELNAME@/Conceptueel Informatiemodel Officiële Publicaties/g
-s/@TITLE@/Conceptueel Informatiemodel Officiële Publicaties/g
-s/@REPONAME@/dso-cim-op/g
--- process-template.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#
-# Script to copy template files to local repository. Remove any file where you wish to keep the local changes.
-#
-cat ../dso-cim-template/README.md.template | sed -f configuration.sed > README.md
-cat ../dso-cim-template/abstract.md.template | sed -f configuration.sed > abstract.md
-cat ../dso-cim-template/imvertor/Imvertor.properties.template | sed -f configuration.sed > imvertor/Imvertor.properties
